bd92be7451774fb09aeaa1c554933eba77b776a3
Anika <anikanayona@gmail.com>
2025-02-26 17:54:45 +0600

    clean up code

--- locators/competitionLoc.py
+++ locators/competitionLoc.py
@@ -1,10 +1,5 @@
 from selenium.webdriver.common.by import By
 
 class competitionelement:
-    DASHBOARD_TITLE = (By.CLASS_NAME, "account-hero-v2__text-block--title")
-    START_CHALLENGE_BUTTON = (By.LINK_TEXT, "Start Challenge")
-    close_button = (By.CLASS_NAME, "ant-notification-notice-close")
-    FNlogo = (By.XPATH, '//*[@alt="fundednext-text-logo"]/following-sibling::*')
-    user = (By.XPATH, '//*[@alt="user"]/following-sibling::*')
-    search = (By.CLASS_NAME, "account-search")
-    intercom = (By.CLASS_NAME, "intercom-lightweight-app-launcher-icon")
+    COMP_TITLE = (By.CLASS_NAME, "competition_hero_container_title")
+   
--- test/competition.py
+++ test/competition.py
@@ -13,5 +13,24 @@ def comp():
     driver = perform__valid_login()
     close_popUp(driver)
 
-    
+    expected_header_title = "FundedNext Trade Master Cup"
+    expected_header_subtitle = "Join the Trade Master Cup, showcase your trading skills, climb the leaderboard, and compete for a $35,000 grand prize with just a $35 entry fee!"
+
+    headertitle = WebDriverWait(driver, 20).until(
+        EC.visibility_of_element_located(myOfferpage.header_title)
+    )
+    assert headertitle.is_displayed(), "title not displayed"
+    actual_header_titile = headertitle.text.strip()
+    assert actual_header_titile == expected_header_title, f"Expected '{expected_header_title}', but found '{actual_header_titile}'"
+
+    headersubtitle = WebDriverWait(driver, 20).until(
+        EC.visibility_of_element_located(myOfferpage.header_subtitle)
+    )
+    assert headersubtitle.is_displayed(), "subtitle not displayed"
+    actual_header_subtitle =  " ".join(headersubtitle.text.split()).strip()
+    assert actual_header_subtitle == expected_header_subtitle, f"Expected '{expected_header_subtitle}', but found '{actual_header_subtitle}'"
+
+
+    intercomicon(driver)
+
     return driver
