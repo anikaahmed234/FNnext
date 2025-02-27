81c3be55c0c4dab71449ce83f7fe83c279dbc250
Anika <anikanayona@gmail.com>
2025-02-27 17:17:27 +0600

    quick fix

--- test/free_Trial.py
+++ test/free_Trial.py
@@ -1,4 +1,5 @@
 
+import time
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
