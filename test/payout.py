c24800199265fbd973f5abdcd921d6c90c55aaf1
Anika <anikanayona@gmail.com>
2025-02-27 15:40:38 +0600

    clean code

--- test/payout.py
+++ test/payout.py
@@ -10,26 +10,27 @@ from .popupsclose import *
 from .intercom import *
 
 def payoutmenu():
-        
+    
     driver = perform__valid_login()
     close_popUp(driver)
 
     #Payout
     Payout = WebDriverWait(driver, 20).until(
-        EC.visibility_of_element_located(SideBar.Payout)
+        EC.visibility_of_element_located(SideBar.Payout) 
     )
     Payout.click(), "sidebar is not visible after login"
-
+    
+    driver.set_page_load_timeout(30)
     time.sleep(3)
 
     expected_url = f'{URL}/payout'
     current_url = driver.current_url
     assert current_url == expected_url, f"Expected URL: {expected_url}, but got: {current_url}"
-    
+        
     expected_col_names = ["Login", "Date", "Withdrawal ID", "Requested Amount", "Status", "Disbursed Amount", "Timer", "Payout Proof","Tx Id","Note"]
 
     cols = WebDriverWait(driver, 20).until(
-        EC.presence_of_all_elements_located((payoutelement.colHead))
+         EC.presence_of_all_elements_located((payoutelement.colHead))
     )
 
     actual_tab_names = [col.text.strip() for col in cols]
@@ -37,12 +38,12 @@ def payoutmenu():
     assert actual_tab_names == expected_col_names, f"Expected tabs {expected_col_names}, but found {actual_tab_names}"
 
     for expected in expected_col_names:
-        assert expected in actual_tab_names, f"Expected tab '{expected}' not found in actual tabs: {actual_tab_names}"
+         assert expected in actual_tab_names, f"Expected tab '{expected}' not found in actual tabs: {actual_tab_names}"
 
     expected_method_names = ["Rise", "USDT", "USDC", "Wind"]
 
     methodnames = WebDriverWait(driver, 20).until(
-        EC.presence_of_all_elements_located((payoutelement.method))
+            EC.presence_of_all_elements_located((payoutelement.method))
     )
 
     actual_method_names = [method.text.strip() for method in methodnames]
@@ -50,6 +51,6 @@ def payoutmenu():
     assert actual_method_names == expected_method_names, f"Expected tabs {expected_method_names}, but found {actual_method_names}"
 
     for expected in expected_method_names:
-        assert expected in actual_method_names, f"Expected tab '{expected}' not found in actual tabs: {actual_method_names}"
-
+           assert expected in actual_method_names, f"Expected tab '{expected}' not found in actual tabs: {actual_method_names}"
+    
     return driver
